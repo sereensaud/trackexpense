d3a6e5a8275a725b15d83800c726a35f99669288
sereensaud <66210596+sereensaud@users.noreply.github.com>
2020-06-22 23:48:37 +0500

    final commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2 className="headermain" style={{fontWeight: 200, textAlign:"center", padding: 2}}>
+         TRACK YOUR EXPENSES 
+        </h2>
+    )
+}
